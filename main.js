ecd7d063e5ae1a10c31ff1f93644cfd7e5b370ef
Emily Middleton <emiddleton@smith.edu>
2025-12-16 20:52:58 -0500

    locked interactions for mistakes, added dropdown menu, removed 'current' highlighting

--- main.js
+++ main.js
@@ -1,29 +1,101 @@
 // https://www.youtube.com/watch?v=jfYWwQrtzzY
 // https://www.youtube.com/watch?v=EFJg4nUg9sM chess example
 // https://www.youtube.com/watch?v=wv7pvH1O5Ho sortable list example
+// https://www.youtube.com/watch?v=hBbrGFCszU4 dropdown menu example
 
+
+/** Dropdown menu functionality */
+const dropdown = document.querySelector('.dropdown');
+
+/** get internal elements */
+const select = dropdown.querySelector('.select');
+const caret = dropdown.querySelector('.caret');
+const menu = dropdown.querySelector('.menu');
+const options = dropdown.querySelectorAll('.menu li');
+const selected = dropdown.querySelector('.selected');
+
+select.addEventListener('click', () => {
+  // add styling to the selected element
+  select.classList.toggle('select-clicked');
+  // add rotate to caret
+  caret.classList.toggle('caret-rotate');
+  menu.classList.toggle('menu-open');
+});
+
+// add event listeners to all options
+options.forEach(option => {
+  // add click listener to each option
+  option.addEventListener('click', () => {
+    // make sure old results are cleared out
+    reset();
+
+    // create an array of the appropriate length
+    createArray(parseInt(option.innerText));
+
+    // change selected inner text to the clicked option
+    selected.innerText = "Array Length: " + option.innerText;
+    // add clicked style to selected object
+    select.classList.remove('select-clicked');
+    caret.classList.remove('caret-rotate');
+    menu.classList.remove('menu-open');
+
+    // remove active class
+    options.forEach(option => {
+      option.classList.remove('active');
+    });
+    // add active to the clicked object only
+    option.classList.add('active');
+  });
+});
+
+
+/** Sorting array functionality */
 const draggableList = document.getElementById('array');
 const indexList = document.getElementById('indexArray');
 const button = document.getElementById('testbutton');
-let lastSwap = [];
-let steps = 0;
-const numValues = 5;
-
 const randomValues = [];
-
-// create a random array of 6 integers
-for (let i = 0; i < numValues; i++) {
-  randomValues[i] = Math.floor(Math.random() * 60); // gives an integer from 0 - 19
-}
-randomValues.sort((a, b) => a - b);
-
 const listVals = [];
 const indexVals = [];
-let dragStartIndex; // changes later on
 
-createList();
-createIndexList();
-listVals[0].classList.add('current'); // automatically the 0 index is 'current'
+let dragStartIndex; // changes later on
+let numValues;
+let lastSwap = [];
+let steps;
+
+/**
+ * clear out old arrays when new ones are requested
+ */
+function reset() {
+  steps = 0;
+  // clear out old arrays in place
+  randomValues.length = 0;
+  // clear out elements from DOM
+  listVals.forEach(element => {
+    element.remove();
+  })
+  indexVals.forEach(index => {
+    index.remove();
+  })
+
+  listVals.length = 0;
+  indexVals.length = 0;
+}
+
+/**
+ * given a number of elements in the array
+ * create a random array of numbers to sort
+ */
+function createArray(optionNumber) {
+  numValues = optionNumber; // from dropdown menu
+
+  // create a random array of 6 integers
+  for (let i = 0; i < numValues; i++) {
+    randomValues[i] = Math.floor(Math.random() * 60); // gives an integer from 0 - 19
+  }
+  randomValues.sort((a, b) => a - b);
+  createList();
+  createIndexList();
+}
 
 // insert list items into dom
 // copy it
@@ -37,7 +109,6 @@ function createList() {
     .sort((a, b) => a.sort - b.sort) // goes by randomized sorting val to scramble array
     .map(a => a.value)
     .forEach((num, index) => {
-
       const listVal = document.createElement('li'); // list item element
 
       listVal.setAttribute('data-index', index); // custom attribute is data-attribute
@@ -87,12 +158,9 @@ function dragDrop() {
   grade(); // call to grade function each time an object is dropped
   if (listVals[steps].classList.contains('right')){
     steps++; // move forward
-    moveCurrSpot(steps);
   } else {
-    // wrong needs to be first, otherwise not colored red
-    listVals[steps].classList.remove('current');
-    listVals[steps].classList.add('wrong');
-    listVals[steps].classList.add('current'); 
+    lockInteraction(); // locks interaction until fixed
+    button.classList.add('locked');
   }
 }
 
@@ -116,15 +184,12 @@ function swapItems(fromIndex, toIndex) {
 
 function undo() {
   swapItems(lastSwap[1], lastSwap[0]);
-  const oldVal = listVals[lastSwap[0]];
-  const oldIdx = indexVals[lastSwap[0]];
-  console.log(oldVal.classList);
-  oldVal.classList.remove('right');
-  oldVal.classList.remove('wrong');
-  oldIdx.classList.remove('sorted');
-  steps--;
-  listVals[steps+1].classList.remove('current');
-  listVals[steps].classList.add('current');
+  const oldVal1 = listVals[lastSwap[0]];
+  const oldVal2 = listVals[lastSwap[1]];
+  oldVal1.classList.remove('wrong');
+  oldVal2.classList.remove('wrong');
+  unlockInteraction();
+  button.classList.remove('locked');
 }
 
 function dragEnter() {
@@ -140,8 +205,8 @@ function grade() {
   const listItem = listVals[steps];
   const itemVal = listItem.querySelector('.draggable').innerText.trim();
   if(parseInt(itemVal) !== randomValues[steps]) {
-    listItem.classList.remove('right');
-    listItem.classList.add('wrong');
+    listVals[lastSwap[0]].classList.add('wrong');
+    listVals[lastSwap[1]].classList.add('wrong');
   } else {
     listItem.classList.remove('wrong');
     listItem.classList.add('right');
@@ -157,11 +222,6 @@ function grade() {
   if (steps === listVals.length-1) {
     markFinished();
   }
-}
-
-function moveCurrSpot(currentIndex) {
-  listVals[currentIndex-1].classList.remove('current');
-  listVals[currentIndex].classList.add('current');
 }
 
 function addEventListeners() {
@@ -186,10 +246,27 @@ function delay(ms) {
 }
 
 async function markFinished() {
-  for (let i = 0; i <= steps; i++){
-    await delay(500);
+  for (let i = 0; i < listVals.length; i++){
+    await delay(250);
     listVals[i].classList.remove('done');
     listVals[i].removeEventListener('dragstart', dragStart);
+  }
+}
+
+// remove event listeners from indices step - endOfArr
+function lockInteraction() {
+  for (let i = steps; i < listVals.length; i++) {
+    listVals[i].removeEventListener('dragstart', dragStart);
+    listVals[i].removeEventListener('drop', dragDrop);
+    listVals[i].removeEventListener('dragover', dragOver);
+  }
+}
+
+function unlockInteraction() {
+  for (let i = steps; i < listVals.length; i++) {
+    listVals[i].addEventListener('dragstart', dragStart);
+    listVals[i].addEventListener('drop', dragDrop);
+    listVals[i].addEventListener('dragover', dragOver);
   }
 }
 
